21ad74eaa596fda2496f9a0e741050caa5b070c6
Karl Hartinger <karl@hartingers.at>
2017-04-09 16:55:08 +0200

    Simple oop example for digital io V2
    oop example for digital io: 2 buttons, 3 leds

---
+++ D1_oop01_DIO2/D1_oop01_DIO2.ino
@@ -0,0 +1,46 @@
+//_____D1_oop01_DIO2.ino___________________________20170402_____
+// Function: Red LED lights up for 1,5s, green LED 0,5s, ...
+//           If button D5 is pressed, both LEDs are on
+//           (=> yellow, if DUO-LED is used).
+//           Blue led (D4) shows inverted state of Button D3
+//           (blue led lights, when input is 0)
+// Hardware: (1) WeMos D1 mini
+//           (2) D1_IO_Shield1: 2x LEDs at D7 (green), D8 (red)
+//                              1x Button at D5
+#include "D1_class_Din.h"
+#include "D1_class_Dout.h"
+Din button3;
+Din button5(D5);
+Dout led_blue;
+Dout led_green(D7);
+Dout led_red(D8);
+int state=0;
+
+void setup() {
+ led_red.on();
+}
+
+void loop() {
+ led_blue.set(button3.get());     //button D3 -> blue led
+ if(button5.isPressed())
+ {
+  led_red.on();
+  led_green.on();
+ }
+ else //=====button D5 not pressed==============================
+ {
+  if((state++)>20) state=0;
+  if(state<15)
+  {
+   led_red.on();
+   led_green.off();
+  }
+  else
+  {
+   led_red.off();
+   led_green.on();
+  }
+ }
+ delay(100);
+
+}
