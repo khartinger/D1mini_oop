c4d65ee2e58bba8ec4d2074a09dd304b586d74c3
Karl Hartinger <karl@hartingers.at>
2017-12-03 20:43:27 +0100

    Demo: class for digital output
    Demo: class for digital output

---
+++ D1_oop01_Dout1/D1_oop01_Dout1.ino
@@ -0,0 +1,47 @@
+//_____D1_oop01_Dout1.ino_____________________171203-171203_____
+// Function: Demonstrate methods of class Dout:
+// * turn blue led on 2 sec, off 2 sec
+// * toggle led every 0,5s and print duration every 0,1 s 
+//   on Serial (115200 Baud).
+//
+// Hardware: (1) WeMos D1 mini
+//
+// Created by Karl Hartinger, December 03, 2017.
+// Released into the public domain.
+
+#include "D1_class_Dout.h"
+#define BAUDRATE             115200    // for Serial
+Dout led_blue(true);                   // blue LED (default D4)
+
+void setup() {
+ //-----begin Serial, read any existing Serial data-------------
+ if(!Serial) Serial.begin(BAUDRATE);   // init serial comm
+ while(!Serial) {;};                   // wait for Serial
+ do{delay(10);} while(Serial.available() && Serial.read()>=0);
+ Serial.println();                     // new line
+ //-----start led output---------------------------------------- 
+ led_blue.on();                        // turn blue LED on
+ Serial.println("Blue LED on");        // note it on Serial
+ delay(1000);                          // wait 1 sec
+ led_blue.set(0);                      // blue LED further on(!)
+ delay(1000);                          // wait 1 sec
+ led_blue.off();                       // turn blue LED off
+ Serial.println("Blue LED off");       // note it on Serial
+ delay(2000);                          // wait 2 sec
+}
+
+int zsec=0, zsmax=10;
+void loop() {
+ //-----toggle led----------------------------------------------
+ if((zsec==0)||(zsec==5)) {            // every 0 and 0,5 s
+  led_blue.toggle();                   // toggle led
+  Serial.print("*T* ");                // print toggle to Serial
+ }
+ //-----print duration------------------------------------------
+ Serial.print(led_blue.getDuration()); // duration of led on/off
+ Serial.print(" ");                    // blank between values
+ //-----increment 0,1s and wait---------------------------------
+ zsec++;                               // increment 0,1s counter
+ if(zsec>=zsmax) { zsec=0;  Serial.println(); } // reset counter
+ delay(100);
+}
